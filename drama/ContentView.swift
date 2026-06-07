5f9d0439fcd208c74d478119835e7c5e7ebbbf81
chenanqi1024 <chenanqi1024@gmail.com>
2026-06-07 21:13:27 +0800

    Initial Commit

---
+++ drama/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  drama
+//
+//  Created by chenanqi on 2026/6/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ drama/dramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  dramaApp.swift
+//  drama
+//
+//  Created by chenanqi on 2026/6/7.
+//
+
+import SwiftUI
+
+@main
+struct dramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
